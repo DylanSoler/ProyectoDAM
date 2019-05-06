5b49208d0bbcb16d3e5f30dc98c7c33c66d1cdb0
DylanSoler <36535979+DylanSoler@users.noreply.github.com>
2019-05-06 13:32:44 +0200

    Update EntrenoController.cs

--- Api/ApiRestFTM/ApiRestFTM-API/Controllers/EntrenoController.cs
+++ Api/ApiRestFTM/ApiRestFTM-API/Controllers/EntrenoController.cs
@@ -10,10 +10,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiRestFTM_API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Manager/{id}/[controller]")]
     [ApiController]
     public class EntrenoController : ControllerBase
     {
+        [HttpGet]
+		public ActionResult Get(int id)
+		{
+            String accept = Request.Headers["Accept"].ToString();
+            clsListadoEntrenos oListEntrenos = new clsListadoEntrenos();
+            List<clsEntreno> oEntrenos = new List<clsEntreno>();
+            
+            if (accept != "application/json" && accept != "*/*")
+                return StatusCode(406); //Not Acceptable
+
+            oEntrenos = oListEntrenos.listadoCompletoEntrenos(id);
+
+			if (oEntrenos.Count == 0)
+				return NotFound(id); //404
+			else
+				return Ok(oEntrenos); //200
+		}
         
     }
 }
