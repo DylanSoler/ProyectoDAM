6e624096fd9e6964fcb4e8bc40a84e38156e62b2
DylanSoler <36535979+DylanSoler@users.noreply.github.com>
2019-05-13 09:16:21 +0200

    Update ManagerController.cs

--- Api/ApiRestFTM/ApiRestFTM-API/Controllers/ManagerController.cs
+++ Api/ApiRestFTM/ApiRestFTM-API/Controllers/ManagerController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiRestFTM_API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ManagerController : ControllerBase
     {
