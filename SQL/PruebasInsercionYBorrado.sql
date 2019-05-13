6a6aee0749e2c47fb3afe10abaddc4a50454e072
Dylan <dylanadriansoler@gmail.com>
2019-05-13 15:44:36 +0200

    Ampliacion docu y modificacion sql pruebas

--- SQL/PruebasInsercionYBorrado.sql
+++ SQL/PruebasInsercionYBorrado.sql
@@ -4,42 +4,45 @@
 ----------------------------------------------------------INSERCION--------------------------------------------------------------
 
 INSERT INTO Managers(Correo,PasswordManager, Nombre, Apellidos, FotoPerfil, FechaNacimiento)
-				VALUES('dylan@gmail.com',CAST('5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' AS BINARY)
+				--VALUES('dylan@gmail.com',CAST('C0E7AEC81A1A194E9F54F6B297F6544041188C741AE8F55C2133B5C510A7DC1D' AS BINARY)
+				--		,'Dylan','Soler Patino','/fotazas/micareto.png',NULL)
+						VALUES('dylan@gmail.com',CONVERT(BINARY(64),'C0E7AEC81A1A194E9F54F6B297F6544041188C741AE8F55C2133B5C510A7DC1D')
 						,'Dylan','Soler Patino','/fotazas/micareto.png',NULL)
 				
 SELECT * FROM Managers
-
 ---------------------------------------------------------------------------------------------------------------------------------
 
 INSERT INTO Entrenos(ID_Manager,Dia,Sesion1,Sesion2,SesionExtra)
-				VALUES	(2,'Lunes','Fisico','Recuperacion','Descanso'),
-						(2,'Martes','Defensivo','Tactico','Descanso'),
-						(2,'Miercoles','Ofensivo','Tecnico','Descanso'),
-						(2,'Jueves','Balon parado','Control','Descanso'),
-						(2,'Viernes','Tacticas prepartido','Recuperacion','Descanso'),
-						(2,'Sabado','','',''),
-						(2,'Domingo','','','')
+				VALUES	(5,'Lunes','Fisico','Recuperacion','Descanso'),
+						(5,'Martes','Defensivo','Tactico','Descanso'),
+						(5,'Miercoles','Ofensivo','Tecnico','Descanso'),
+						(5,'Jueves','Balon parado','Control','Descanso'),
+						(5,'Viernes','Tacticas prepartido','Recuperacion','Descanso'),
+						(5,'Sabado','','',''),
+						(5,'Domingo','','','')
 						
 SELECT * FROM Entrenos
 
 ---------------------------------------------------------------------------------------------------------------------------------
 
 INSERT INTO Notas(ID_Manager, Titulo, TextoNota)
-				VALUES(2,'Retrasos','El jugador Luis Z ha llegado tarde hoy otra vez, como de costumbre')
+				VALUES(5,'Retrasos','El jugador Luis Z ha llegado tarde hoy otra vez, como de costumbre'),
+					  (5,'Entenos','Aumentar con sesion fisica')
 				
 SELECT * FROM Notas
+DELETE FROM NOTAS --WHERE ID_Manager=5 AND ID_nota=8
 
 ---------------------------------------------------------------------------------------------------------------------------------
 
 INSERT INTO ManagersTacticas(ID_Manager,ID_Tactica,Mentalidad,Descripcion)
-								VALUES(2,5,'Ofensiva','Juego de posesión, con presión constante alta y verticalidad ofensiva')
+								VALUES(5,5,'Ofensiva','Juego de posesión, con presión constante alta y verticalidad ofensiva')
 								
 SELECT * FROM ManagersTacticas
 
 
 -----------------------------------------------------------BORRADO---------------------------------------------------------------
 
-DELETE FROM Managers WHERE ID=2
+DELETE FROM Managers WHERE ID=5
 
 SELECT * FROM Managers
 SELECT * FROM ManagersTacticas
