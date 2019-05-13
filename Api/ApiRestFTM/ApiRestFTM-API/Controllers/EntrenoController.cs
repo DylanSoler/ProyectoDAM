d5e6ca046511181d9c3a186726292a7781d11f23
DylanSoler <36535979+DylanSoler@users.noreply.github.com>
2019-05-13 09:15:37 +0200

    Update EntrenoController.cs

--- Api/ApiRestFTM/ApiRestFTM-API/Controllers/EntrenoController.cs
+++ Api/ApiRestFTM/ApiRestFTM-API/Controllers/EntrenoController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiRestFTM_API.Controllers
 {
-    [Route("api/Manager/{id}/[controller]")]
+    [Route("Manager/{id}/[controller]")]
     [ApiController]
     public class EntrenoController : ControllerBase
     {
