60e8bdf3820ba5f6553e81e8456897f7f3981b41
DylanSoler <36535979+DylanSoler@users.noreply.github.com>
2019-05-13 09:50:36 +0200

    Update NotaController.cs

--- Api/ApiRestFTM/ApiRestFTM-API/Controllers/NotaController.cs
+++ Api/ApiRestFTM/ApiRestFTM-API/Controllers/NotaController.cs
@@ -10,10 +10,30 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiRestFTM_API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("Manager/{id}/[controller]")]
     [ApiController]
     public class NotaController : ControllerBase
     {
+        //GET Manager/{id}/nota
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            String accept = Request.Headers["Accept"].ToString();
+            clsListadoNotas oListNotas = new clsListadoNotas();
+            List<clsNota> oNotas = new List<clsNota>();
+
+            if (accept != "application/json" && accept != "*/*")
+                return StatusCode(406); //Not Acceptable
+            else
+            {
+                oNotas = oListNotas.listadoCompletoEntrenos(id);
+
+                if (oEntrenos.Count == 0)
+                return NotFound(id); //404
+                else
+                return Ok(oEntrenos); //200
+            }
+        }
         
     }
 }
