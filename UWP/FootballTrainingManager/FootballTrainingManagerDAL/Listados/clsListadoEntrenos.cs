53032288a9a1810926096ff8cf40c5b6df6201a6
DylanSoler <36535979+DylanSoler@users.noreply.github.com>
2019-05-14 17:01:03 +0200

    Update clsListadoEntrenos.cs

--- UWP/FootballTrainingManager/FootballTrainingManagerDAL/Listados/clsListadoEntrenos.cs
+++ UWP/FootballTrainingManager/FootballTrainingManagerDAL/Listados/clsListadoEntrenos.cs
@@ -9,7 +9,32 @@ using FootballTrainingManagerEntidades.Persistencia;
 
 namespace FootballTrainingManagerDAL.Listados
 {
-    class clsListadoEntrenos
+    public class clsListadoEntrenos
     {
+        
+        /// <summary>
+        /// Metodo que devuelve el listado completo de entrenos
+        /// </summary>
+        /// <returns>List de clsEntreno</returns>
+        public async Task<List<clsEntreno>> listadoCompletoEntrenosDAL(int idManager)
+        {
+            clsUriBase uribase = new clsUriBase();
+            String ruta = uribase.getUriBaseApi();
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            List<clsEntreno> listado = new List<clsEntreno>();
+
+            //try catch, throw ex 
+            HttpResponseMessage response = await client.GetAsync($"{ruta}manager/{idManager}/entreno");
+
+            if (response.IsSuccessStatusCode){
+                string lista = await response.Content.ReadAsStringAsync();
+                listado = JsonConvert.DeserializeObject<List<clsEntreno>>(lista);
+            }
+            
+            return listado;
+        }
+        
     }
 }
