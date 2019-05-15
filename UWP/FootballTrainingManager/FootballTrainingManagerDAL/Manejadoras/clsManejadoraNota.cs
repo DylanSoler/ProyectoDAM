d6ce159167808bdfd91fcbfbc25e9479ad142dc2
DylanSoler <36535979+DylanSoler@users.noreply.github.com>
2019-05-15 13:15:45 +0200

    Update clsManejadoraNota.cs

--- UWP/FootballTrainingManager/FootballTrainingManagerDAL/Manejadoras/clsManejadoraNota.cs
+++ UWP/FootballTrainingManager/FootballTrainingManagerDAL/Manejadoras/clsManejadoraNota.cs
@@ -9,7 +9,154 @@ using FootballTrainingManagerEntidades.Persistencia;
 
 namespace FootballTrainingManagerDAL.Manejadoras
 {
-    class clsManejadoraNota
+    public class clsManejadoraNota
     {
+        
+        /// <summary>
+        /// Metodo que devuelve una nota de un manager segun id del manager e id nota
+        /// </summary>
+        /// <param name="idManager">id del manager</param>
+        /// <param name="idNota">id de la nota</param>
+        /// <returns>clsNota</returns>
+        public async Task<clsNota> obtenerNotaPorIDDAL(int idManager, int idNota)
+        {
+            clsUriBase uribase = new clsUriBase();
+            String ruta = uribase.getUriBaseApi();
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            
+            clsNota nota = new clsNota();
+
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            try {
+                response = await client.GetAsync($"{ruta}manager/{idManager}/nota/{idNota}");
+            } catch (Exception ex) {
+                
+            }
+
+            if (response.IsSuccessStatusCode){
+                string ent = await response.Content.ReadAsStringAsync();
+                nota = JsonConvert.DeserializeObject<clsNota>(ent);
+            }
+
+            return nota;
+        }
+        
+        /// <summary>
+        /// Metodo que actualiza una nota de un manager
+        /// </summary>
+        /// <param name="nota">nota a actualizar</param>
+        /// <returns>boolean true si todo va bien, false si no</returns>
+        public async Task<boolean> actualizarNotaDAL(clsNota nota)
+        {
+            HttpClient client = new HttpClient();
+            clsUriBase uribase = new clsUriBase();
+            String ruta = uribase.getUriBaseApi();
+            String datos;
+            HttpContent contenido;
+            Uri miUri = new Uri($"{ruta}manager/{nota.idManager}/nota");
+            boolean ret = false;
+
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            try{
+                datos = JsonConvert.SerializeObject(nota);
+                contenido = new StringContent(datos, System.Text.Encoding.UTF8, "application/json");
+                response = await client.PutAsync(miUri, contenido);
+            } catch (Exception ex) {
+                
+            }
+
+            if (response.IsSuccessStatusCode)
+                ret = true;
+
+            return ret;
+        }
+        
+        /// <summary>
+        /// Metodo que inserta una nota de un manager
+        /// </summary>
+        /// <param name="nota">nota a insertar</param>
+        /// <returns>boolean true si todo va bien, false si no</returns>
+        public async Task<boolean> insertarNotaDAL(clsNota nota)
+        {
+            HttpClient client = new HttpClient();
+            clsUriBase uribase = new clsUriBase();
+            String ruta = uribase.getUriBaseApi();
+            String datos;
+            HttpContent contenido;
+            Uri miUri = new Uri($"{ruta}manager/{nota.idManager}/nota");
+            boolean ret = false;
+
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            try{
+                datos = JsonConvert.SerializeObject(nota);
+                contenido = new StringContent(datos, System.Text.Encoding.UTF8, "application/json");
+                response = await client.PostAsync(miUri, contenido);
+            } catch (Exception ex) {
+                
+            }
+
+            if (response.IsSuccessStatusCode)
+                ret = true;
+
+            return ret;
+        }
+        
+        /// <summary>
+        /// Metodo que elimina una nota segun id del manager e id de la nota
+        /// </summary>
+        /// <param name="idManager">id del manager</param>
+        /// <param name="idNota">id de la nota</param>
+        /// <returns>boolean true si todo va bien, false si no</returns>
+        public async Task<boolean> eliminarNotaDAL(int idManager, int idNota)
+        {
+            HttpClient client = new HttpClient();
+            clsUriBase uribase = new clsUriBase();
+            String ruta = uribase.getUriBaseApi();
+            boolean ret = false;
+
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            try{
+                response = await client.DeleteAsync($"{ruta}manager/{idManager}/nota/{idNota}");
+            } catch (Exception ex) {
+                
+            }
+
+            if (response.IsSuccessStatusCode)
+                ret = true;
+
+            return ret;
+        }
+        
+        /// <summary>
+        /// Metodo que elimina todas las notas de un manager
+        /// </summary>
+        /// <param name="idManager">id del manager</param>
+        /// <returns>boolean true si todo va bien, false si no</returns>
+        public async Task<boolean> eliminarTodasLasNotasDAL(int idManager)
+        {
+            HttpClient client = new HttpClient();
+            clsUriBase uribase = new clsUriBase();
+            String ruta = uribase.getUriBaseApi();
+            boolean ret = false;
+
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            try{
+                response = await client.DeleteAsync($"{ruta}manager/{idManager}/nota/");
+            } catch (Exception ex) {
+                
+            }
+
+            if (response.IsSuccessStatusCode)
+                ret = true;
+
+            return ret;
+        } 
+        
     }
 }
